759bb3cceaa81965a2313e132e04b25eb7d90acd
MohammedJawad1920~ <ksdjabbu@gmail.com>
2024-01-23 11:55:13 +0530

    middleware chenged

--- Next-Projects/sdc/middleware.js
+++ Next-Projects/sdc/middleware.js
@@ -2,42 +2,33 @@ import { jwtVerify } from "jose";
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  const libraryToken = request.cookies.get("libraryToken");
-  const festToken = request.cookies.get("festToken");
-
-  const books = request.url.startsWith(
-    `${process.env.BASE_URL}/sdc-dashboard/books`
-  );
-  const rental = request.url.startsWith(
-    `${process.env.BASE_URL}/sdc-dashboard/rental`
-  );
-  const students = request.url.startsWith(
-    `${process.env.BASE_URL}/sdc-dashboard/students`
-  );
-  const userManagement = request.url.startsWith(
-    `${process.env.BASE_URL}/sdc-dashboard/user-management`
-  );
-
-  const isSdcDashBoard = request.url.startsWith(
-    `${process.env.BASE_URL}/sdc-dashboard`
-  );
-
-  const isMeeladFest = request.url.startsWith(
-    `${process.env.BASE_URL}/meelad-fest`
-  );
-
-  if ((!libraryToken && isSdcDashBoard) || (!festToken && isMeeladFest)) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  let token;
-  if (isSdcDashBoard) {
-    token = libraryToken.value;
-  } else {
-    token = festToken.value;
-  }
-
   try {
+    const libraryToken = request.cookies.get("libraryToken");
+    const festToken = request.cookies.get("festToken");
+
+    const isSdcDashBoard = request.url.startsWith(
+      `${process.env.BASE_URL}/sdc-dashboard`
+    );
+
+    const isMeeladFest = request.url.startsWith(
+      `${process.env.BASE_URL}/meelad-fest`
+    );
+
+    if ((!libraryToken && isSdcDashBoard) || (!festToken && isMeeladFest)) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    let token;
+    if (isSdcDashBoard) {
+      token = libraryToken?.value;
+    } else {
+      token = festToken?.value;
+    }
+
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+
     const { payload } = await jwtVerify(
       token,
       new TextEncoder().encode(process.env.JWT_SECRET_KEY)
@@ -54,16 +45,37 @@ export async function middleware(request) {
     }
 
     if (
-      (userManagement && !administrator) ||
-      ((books || userManagement || rental || students) && subscriber)
+      (payload.accessLocation === "Library" && !isSdcDashBoard) ||
+      (payload.accessLocation === "Meelad Fest" && !isMeeladFest)
+    ) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    if (
+      (request.url.startsWith(
+        `${process.env.BASE_URL}/sdc-dashboard/user-management`
+      ) &&
+        !administrator) ||
+      (subscriber &&
+        (request.url.startsWith(
+          `${process.env.BASE_URL}/sdc-dashboard/books`
+        ) ||
+          request.url.startsWith(
+            `${process.env.BASE_URL}/sdc-dashboard/rental`
+          ) ||
+          request.url.startsWith(
+            `${process.env.BASE_URL}/sdc-dashboard/students`
+          )))
     ) {
       return NextResponse.redirect(new URL("/404", request.url));
     }
 
     return NextResponse.next();
   } catch (error) {
-    if (error instanceof jose.JWTPayloadError) {
-      // Handle JWT payload errors, such as invalid or expired tokens
+    console.error("Error in middleware:", error);
+
+    if (error instanceof jwt.errors.JWTError) {
+      // Handle JWT errors, such as invalid or expired tokens
       return NextResponse.redirect(new URL("/login", request.url));
     } else {
       // Handle other errors, such as network issues or missing env variables
