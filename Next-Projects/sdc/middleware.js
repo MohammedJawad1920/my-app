35a7f207664384cbdccdbc82274daae15171bb89
MohammedJawad1920~ <ksdjabbu@gmail.com>
2024-01-23 12:09:35 +0530

    middleware chenged

--- Next-Projects/sdc/middleware.js
+++ Next-Projects/sdc/middleware.js
@@ -1,92 +1,62 @@
+// middleware.js
 import { jwtVerify } from "jose";
 import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken"; // Import the jsonwebtoken package
 
 export async function middleware(request) {
+  const libraryToken = request.cookies.get("libraryToken");
+  const festToken = request.cookies.get("festToken");
+
+  const books = request.url.startsWith(
+    `${process.env.BASE_URL}/sdc-dashboard/books`
+  );
+  const rental = request.url.startsWith(
+    `${process.env.BASE_URL}/sdc-dashboard/rental`
+  );
+  const students = request.url.startsWith(
+    `${process.env.BASE_URL}/sdc-dashboard/students`
+  );
+  const userManagement = request.url.startsWith(
+    `${process.env.BASE_URL}/sdc-dashboard/user-management`
+  );
+
+  const isSdcDashBoard = request.url.startsWith(
+    `${process.env.BASE_URL}/sdc-dashboard`
+  );
+
+  const isMeeladFest = request.url.startsWith(
+    `${process.env.BASE_URL}/meelad-fest`
+  );
+
+  if ((!libraryToken && isSdcDashBoard) || (!festToken && isMeeladFest)) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  const { payload } = await jwtVerify(
+    isSdcDashBoard ? libraryToken.value : festToken.value,
+    new TextEncoder().encode(process.env.JWT_SECRET_KEY)
+  );
+
+  const administrator = payload.role === "Administrator";
+  const subscriber = payload.role === "Subscriber";
+  const library = payload.accessLocation === "Library";
+  const meeladFest = payload.accessLocation === "Meelad Fest";
+  const attendance = payload.accessLocation === "Attendance";
+
+  if ((library && !isSdcDashBoard) || (meeladFest && !isMeeladFest)) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (
+    (userManagement && !administrator) ||
+    ((books || userManagement || rental || students) && subscriber)
+  ) {
+    return NextResponse.redirect(new URL("/404", request.url));
+  }
+
   try {
-    const libraryToken = request.cookies.get("libraryToken");
-    const festToken = request.cookies.get("festToken");
-
-    const isSdcDashBoard = request.url.startsWith(
-      `${process.env.BASE_URL}/sdc-dashboard`
-    );
-
-    const isMeeladFest = request.url.startsWith(
-      `${process.env.BASE_URL}/meelad-fest`
-    );
-
-    if ((!libraryToken && isSdcDashBoard) || (!festToken && isMeeladFest)) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-
-    let token;
-    if (isSdcDashBoard) {
-      token = libraryToken?.value;
-    } else {
-      token = festToken?.value;
-    }
-
-    if (!token) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-
-    const { payload } = await jwtVerify(
-      token,
-      new TextEncoder().encode(process.env.JWT_SECRET_KEY),
-      {
-        algorithms: ["HS256"], // Specify the algorithm you are using
-      }
-    );
-
-    const administrator = payload.role === "Administrator";
-    const subscriber = payload.role === "Subscriber";
-    const library = payload.accessLocation === "Library";
-    const meeladFest = payload.accessLocation === "Meelad Fest";
-    const attendance = payload.accessLocation === "Attendance";
-
-    if ((library && !isSdcDashBoard) || (meeladFest && !isMeeladFest)) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-
-    if (
-      (payload.accessLocation === "Library" && !isSdcDashBoard) ||
-      (payload.accessLocation === "Meelad Fest" && !isMeeladFest)
-    ) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-
-    if (
-      (request.url.startsWith(
-        `${process.env.BASE_URL}/sdc-dashboard/user-management`
-      ) &&
-        !administrator) ||
-      (subscriber &&
-        (request.url.startsWith(
-          `${process.env.BASE_URL}/sdc-dashboard/books`
-        ) ||
-          request.url.startsWith(
-            `${process.env.BASE_URL}/sdc-dashboard/rental`
-          ) ||
-          request.url.startsWith(
-            `${process.env.BASE_URL}/sdc-dashboard/students`
-          )))
-    ) {
-      return NextResponse.redirect(new URL("/404", request.url));
-    }
-
     return NextResponse.next();
   } catch (error) {
-    console.error("Error in middleware:", error);
-
-    if (error instanceof jwt.JsonWebTokenError) {
-      // Handle JWT errors, such as invalid or expired tokens
-      return NextResponse.redirect(new URL("/login", request.url));
-    } else {
-      // Handle other errors, such as network issues or missing env variables
-      return NextResponse.redirect(new URL("/500", request.url), {
-        status: 500,
-      });
-    }
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 }
 
