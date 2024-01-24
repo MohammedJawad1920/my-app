2b23dea5e82e7eb16683d7dab0dd69bb5d39fce0
MohammedJawad1920~ <ksdjabbu@gmail.com>
2024-01-24 17:19:05 +0530

    Changed

--- Next-Projects/sdc/components/UserDetailsContainer.jsx
+++ Next-Projects/sdc/components/UserDetailsContainer.jsx
@@ -69,7 +69,7 @@ const UserDetails = ({ data }) => {
     const token = Cookies.get("sdcToken");
     try {
       await axios.post("/api/users", { _id, token });
-      // fetchUserData();
+      fetchUserData();
       handleCloseDialog();
     } catch (err) {
       console.log("Error:", err.response.data.msg);
