143780e7c09c9d570217ae2ad0c523a685d9da67
MohammedJawad1920~ <ksdjabbu@gmail.com>
2024-01-25 15:51:16 +0530

    made some changes

--- Next-Projects/sdc/app/api/books/get/route.js
+++ Next-Projects/sdc/app/api/books/get/route.js
@@ -1,11 +1,27 @@
 import { connectToDB } from "@/libs/connectToDB";
 import Book from "@/models/Book";
 import { NextResponse } from "next/server";
+import { revalidateTag } from "next/cache";
 
 connectToDB();
 
 export async function GET() {
   try {
+    const secret = request.nextUrl.searchParams.get("secret");
+    const tag = request.nextUrl.searchParams.get("tag");
+
+    if (secret !== process.env.SECRET_KEY) {
+      return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
+    }
+
+    if (!tag) {
+      return NextResponse.json(
+        { message: "Missing tag param" },
+        { status: 400 }
+      );
+    }
+
+    revalidateTag(tag);
     const books = await Book.find({});
     return NextResponse.json({ succes: true, books }, { status: 201 });
   } catch (err) {
