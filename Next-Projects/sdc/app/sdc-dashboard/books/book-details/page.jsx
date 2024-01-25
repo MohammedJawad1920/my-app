533f3b67c912883a3be721b602c0bc6ed29b34ec
MohammedJawad1920~ <ksdjabbu@gmail.com>
2024-01-25 17:42:42 +0530

    made some changes

--- Next-Projects/sdc/app/sdc-dashboard/books/book-details/page.jsx
+++ Next-Projects/sdc/app/sdc-dashboard/books/book-details/page.jsx
@@ -1,9 +1,10 @@
 import BookDetailsContainer from "@/components/BookDetailsContainer";
+import axios from "axios";
 export const revalidate = 0;
 export const dynamic = "force-dynamic";
 
 const fetchBookData = async () => {
-  const res = await fetch(`${process.env.BASE_URL}/api/books/get`);
+  const res = await axios.get(`${process.env.BASE_URL}/api/books/get`);
   const data = await res.json();
   const books = await data.books;
   return books;
